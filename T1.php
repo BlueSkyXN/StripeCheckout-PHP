4750a1758f9554abdebf0323fd3869583baf40ac
BlueSkyXN <xienan0714@gmail.com>
2021-11-22 11:05:05 +0800

    1.0

---
+++ T1.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';
+\Stripe\Stripe::setApiKey('sk_live_XXX');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'https://pay.XXX.com/';
+
+$checkout_session = \Stripe\Checkout\Session::create([
+  'submit_type' => 'auto',
+  'billing_address_collection' => 'auto',
+  'line_items' => [[
+    'price' => 'price_XXXXXXx',
+    'quantity' => 1,
+    'adjustable_quantity' => [
+        'enabled' => true,
+        'minimum' => 1,
+        'maximum' => 99,
+      ],
+  ]],
+  'payment_method_types' => [
+    'card',
+    'alipay',
+    'wechat_pay',
+  ],
+  'payment_method_options' =>[
+    'wechat_pay' => [
+    'client' => 'web',
+    ],
+  ],
+  'mode' => 'payment',
+  'success_url' => $YOUR_DOMAIN . '/success.html',
+  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+]);
+
+header("HTTP/1.1 303 See Other");
+header("Location: " . $checkout_session->url);
